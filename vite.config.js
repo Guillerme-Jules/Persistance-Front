6b922baa21636cc3261bd5bef29bc8a4ecc597a7
Jules <Jules.guillerme49@hotmail.com>
2026-06-09 14:58:59 +0200

    DEV - Docker

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+  },
+  server: {
+    host: true,           // accessible depuis l'hôte
+    port: 5173,
+    watch: {
+      usePolling: true,   // détecte les changements dans un volume Docker
     },
   },
 })
